9f0b784a0d17973d6d92a0c25a14f3f46d52d78d
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2023-01-15 14:12:06 -0300

    falta corregir error en la carga de las imagenes

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -1,7 +1,7 @@
 import { ImageListItem, ImageList } from '@mui/material';
 
 
-export const ImageGallery = ({ images }) => {
+export const ImageGallery = ({ images = [] }) => {
 
   return (
     <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
