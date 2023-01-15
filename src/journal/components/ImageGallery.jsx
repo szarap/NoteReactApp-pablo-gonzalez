d1ae54a843408225f45f6455ea736a9bc0b287ad
Pablo Gonzalez <lic.szgonzalez@gmail.com>
2023-01-15 14:18:36 -0300

    sigue el error en la carga de imagenes

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -5,7 +5,7 @@ export const ImageGallery = ({ images = [] }) => {
 
   return (
     <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
-      { images.map((image) => (
+      { images?.map((image) => (
         <ImageListItem key={image}>
           <img
             src={`${image}?w=164&h=164&fit=crop&auto=format`}
